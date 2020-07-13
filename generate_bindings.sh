7e6b480b087405178c6852f87b0514f8495dc8c4
Kaashif Hymabaccus <kaashif@kaashif.co.uk>
2020-07-13 21:43:13 +0100

    Fix bindings for FMPQ_POLY_INLINE funcs

--- generate_bindings.sh
+++ generate_bindings.sh
@@ -3,6 +3,7 @@
 mkdir include
 cp -rv /usr/local/include/{flint,antic} include/
 find include -type f -exec sed -i 's/^FMPQ_INLINE//g' {} \;
+find include -type f -exec sed -i 's/^FMPQ_POLY_INLINE//g' {} \;
 find include -type f -exec sed -i 's/^NF_ELEM_INLINE//g' {} \;
 /usr/bin/bindgen include/antic/nf_elem.h -- -Iinclude > src/bindings.rs
 perl -ne 'print if ($. < 20000) or ($_ !~ /FP_(NAN|INFINITE|ZERO|SUBNORMAL|NORMAL)/)' src/bindings.rs | sponge src/bindings.rs
